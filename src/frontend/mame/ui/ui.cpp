60146e09475c2cab8b7721bbe571da9fcd40399d
tomctomc <tomctomc@users.noreply.github.com>
2015-03-10 01:17:03 -0400

    disabled nag screens (i know, i know)

--- src/frontend/mame/ui/ui.cpp
+++ src/frontend/mame/ui/ui.cpp
@@ -409,6 +409,8 @@ void mame_ui_manager::display_startup_screens(bool first_time)
 	bool show_warnings = true, show_mandatory_fileman = true;
 	bool video_none = strcmp(downcast<osd_options &>(machine().options()).video(), OSDOPTVAL_NONE) == 0;
 
+    show_gameinfo = show_warnings = show_mandatory_fileman = false; //TOMCXXX
+
 	// disable everything if we are using -str for 300 or fewer seconds, or if we're the empty driver,
 	// or if we are debugging, or if there's no mame window to send inputs to
 	if (!first_time || (str > 0 && str < 60*5) || &machine().system() == &GAME_NAME(___empty) || (machine().debug_flags & DEBUG_FLAG_ENABLED) != 0 || video_none)
