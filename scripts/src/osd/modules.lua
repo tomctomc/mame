716e5086c9c0d00a0cde4c8beea4b87e8d945690
tomctomc <tomctomc@users.noreply.github.com>
2017-12-03 09:51:35 -0500

    fix for using qtchooser

--- scripts/src/osd/modules.lua
+++ scripts/src/osd/modules.lua
@@ -410,6 +410,9 @@ function qtdebuggerbuild()
 				MOC = MOCTST
 			end
 		end
+		if MOC == "/usr/bin/moc" then  -- TOMCXXX
+			MOC = MOC .. " -qt=5"      -- TOMCXXX
+		end                            -- TOMCXXX
 
 
 		custombuildtask {
